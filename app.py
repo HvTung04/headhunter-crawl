be3d9affd95a8240e3270a7caa95425e12f698ca
Hoang Viet Tung <22022663@vnu.edu.vn>
2025-04-02 13:35:52 +0700

    Apply linkedin filters

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from crawler.Crawler import Crawler
+from sheet_manager.InputSheet import InputSheet
 
 crawler = Crawler()
+input_sheet = InputSheet()
 
 crawler.login_linkedin()
 crawler.search_linkedin_people("python developer", 1)
+crawler.apply_linkedin_filters({"location": ["Hanoi"], "company": ["FPT Software"]})
 print(crawler.get_people_links())
--- crawler/Crawler.py
+++ crawler/Crawler.py
@@ -9,10 +9,11 @@ from dotenv import load_dotenv
 import os
 import json
 
-logging.getLogger('selenium').setLevel(logging.WARNING)
+logging.getLogger("selenium").setLevel(logging.WARNING)
 
 # Load environment variables from .env file
 load_dotenv()
+
 
 class Crawler:
     def __init__(self):
@@ -20,10 +21,10 @@ class Crawler:
         # self.options.add_argument("--headless")
         self.options.add_argument("--log-level=3")
         self.driver = webdriver.Chrome(options=self.options)
-        
+
         # You can now access environment variables like this:
-        self.linkedin_username = os.getenv('LINKEDIN_USERNAME')
-        self.linkedin_password = os.getenv('LINKEDIN_PASSWORD')
+        self.linkedin_username = os.getenv("LINKEDIN_USERNAME")
+        self.linkedin_password = os.getenv("LINKEDIN_PASSWORD")
 
     def login_linkedin(self, username=None, password=None):
         """
@@ -35,10 +36,12 @@ class Crawler:
         """
         username = username or self.linkedin_username
         password = password or self.linkedin_password
-        
+
         if not username or not password:
-            raise ValueError("LinkedIn credentials not found in environment variables or parameters")
-            
+            raise ValueError(
+                "LinkedIn credentials not found in environment variables or parameters"
+            )
+
         self.driver.get("https://www.linkedin.com/login?fromSignIn=true")
         username_field = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.ID, "username"))
@@ -48,7 +51,7 @@ class Crawler:
         password_field.send_keys(password)
         password_field.send_keys(Keys.RETURN)
         time.sleep(5)
-    
+
     def load_cookies(self, cookies_path="cookies.json"):
         with open(cookies_path, "r") as file:
             cookies = json.load(file)
@@ -70,6 +73,108 @@ class Crawler:
         query = query.replace(" ", "%20")
         url = f"https://www.linkedin.com/search/results/people/?keywords={query}&page={page}"
         self.driver.get(url)
+        time.sleep(5)
+
+    def apply_linkedin_filters(self, filters):
+        """
+        Apply filters to the LinkedIn search results.
+        """
+        all_filters_button = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//button[@aria-label="Show all filters. Clicking this button displays all available filter options."]',
+                )
+            )
+        )
+        all_filters_button.click()
+        time.sleep(5)
+        # 0 location
+        # 1 current company
+        # 4 past company
+        for location in filters["location"]:
+            try:
+                filters_button = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_all_elements_located(
+                        (
+                            By.XPATH,
+                            '//button[@class="artdeco-button artdeco-button--muted artdeco-button--2 artdeco-button--tertiary ember-view reusable-search-filters-advanced-filters__add-filter-button"]',
+                        )
+                    )
+                )
+                filters_button[0].click()
+
+                location_input = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located(
+                        (By.XPATH, '//input[@placeholder="Add a location"]')
+                    )
+                )
+                location_input.send_keys(location)
+                time.sleep(3)
+                location_input.send_keys(Keys.DOWN)
+                location_input.send_keys(Keys.RETURN)
+                time.sleep(3)
+            except Exception as e:
+                print(e)
+                continue
+
+        for company in filters["company"]:
+            try:
+                filters_button = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_all_elements_located(
+                        (
+                            By.XPATH,
+                            '//button[@class="artdeco-button artdeco-button--muted artdeco-button--2 artdeco-button--tertiary ember-view reusable-search-filters-advanced-filters__add-filter-button"]',
+                        )
+                    )
+                )
+                filters_button[1].click()
+
+                company_input = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_all_elements_located(
+                        (By.XPATH, '//input[@placeholder="Add a company"]')
+                    )
+                )[0]
+                company_input.send_keys(company)
+                time.sleep(3)
+                company_input.send_keys(Keys.DOWN)
+                company_input.send_keys(Keys.RETURN)
+                time.sleep(3)
+            except Exception as e:
+                print(e)
+                continue
+
+        for company in filters["company"]:
+            try:
+                filters_button = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_all_elements_located(
+                        (
+                            By.XPATH,
+                            '//button[@class="artdeco-button artdeco-button--muted artdeco-button--2 artdeco-button--tertiary ember-view reusable-search-filters-advanced-filters__add-filter-button"]',
+                        )
+                    )
+                )
+                filters_button[1].click()
+
+                company_input = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_all_elements_located(
+                        (By.XPATH, '//input[@placeholder="Add a company"]')
+                    )
+                )[1]
+                self.driver.execute_script("arguments[0].scrollIntoView(true);", company_input)
+                time.sleep(3)
+                company_input.send_keys(company)
+                time.sleep(3)
+                company_input.send_keys(Keys.DOWN)
+                company_input.send_keys(Keys.RETURN)
+                time.sleep(3)
+            except Exception as e:
+                print(e)
+                continue
+
+        # Show results
+        show_results_button = self.driver.find_element(By.XPATH, '//button[@aria-label="Apply current filters to show results"]')
+        show_results_button.click()
         time.sleep(5)
 
     def get_people_links(self):
@@ -97,7 +202,9 @@ class Crawler:
         self.driver.get(f"{profile_link}/details/experience/")
         try:
             latest_company = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, '//div[@data-view-name="profile-component-entity"]//a'))
+                EC.presence_of_element_located(
+                    (By.XPATH, '//div[@data-view-name="profile-component-entity"]//a')
+                )
             )
         except:
             return ""
@@ -137,6 +244,6 @@ class Crawler:
             EC.presence_of_element_located((By.TAG_NAME, "body"))
         )
         return body.text
-        
+
     def stop(self):
         self.driver.close()
